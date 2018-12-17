bf7b84ae4cd00a1ec71b06d38515914a917bf374
NemanjaMandic <nemanja.mandic@folkmatic.com>
2018-12-17 12:27:17 +0100

    added lib

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,32 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+class App extends React.Component<
+{ message: string},
+{count: number}
+>{
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      count: 0
+    }
+  }
+  render(){
+    return(
+      <div onClick={ this.increment }>{this.props.message} { this.state.count }</div>
+    )
+  }
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+}
+
 ReactDOM.render(
-  <div>Zdravo SVet</div>,
+  <App message="Zdravo tamp" />,
   document.getElementById("root")
 );
